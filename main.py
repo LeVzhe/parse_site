f0f5d43d00cb55eb4ff72aeaad3c53e128d60dd1
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-07-27 16:12:25 +0300

    feat: change site for pandora, create parse logic

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL_TEMPLATE = "http://seasonvar.ru/"
-request = requests.get(URL_TEMPLATE)
+URL_TEMPLATE = (
+    "https://time.by/catalog/jewelry/serebryanye-yuvelirnye-ukrasheniya/?PAGEN_2={}"
+)
+page_number = 1
 
-if request.status_code != 200:
-    print("The request status code is not 200. Exiting the program.")
-    exit()
+while True:
+    url = URL_TEMPLATE.format(page_number)
+    r = requests.get(url)
+    if r.status_code != 200:
+        print("The request status code is not 200. Exiting the program.")
+        exit()
 
-soup = BeautifulSoup(request.text, "html.parser")
+    soup = BeautifulSoup(r.text, "html.parser")
 
-site_content = soup.find_all("div", class_="news")
+    jewels = soup.find_all("div", class_="products__item-wrap")
+    last_a = soup.find_all("div", class_="pagination__itemss")[0].find_all("a")[-1]
+    last_pag_page_int = int(last_a.text)
 
-for new in site_content:
-    new_head_class = new.find("div", class_="news-head")
-    a_tags = new.find_all("a")
-    print(new_head_class.text)
-    print("--------------")
-
-    for a_tag in a_tags:
-        str = (
-            "* "
-            + a_tag.find("div", class_="news_n").text.replace("\n", "").strip()
-            + " | "
-            + a_tag.find("span", class_="news_s").text.replace("\n", "").strip()
+    for card in jewels:
+        image_url = (
+            "https://time.by"
+            + card.find("div", class_="products__item-image").a.img["src"]
         )
-        print(str)
+        jewel_name = card.find("div", class_="products__item-title").a.text.strip()
+        jewel_price = card.find("div", class_="products__item-price").text.strip()
+
+    page_number += 1
+    if page_number > 2:  # last_pag_page_int:
+        break
