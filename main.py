084f8389a51442841e9ede306e173e1a91ad2de3
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-07-27 14:56:19 +0300

    fix: refactoring

--- main.py
+++ main.py
@@ -6,19 +6,20 @@ URL_TEMPLATE = "http://seasonvar.ru/"
 request = requests.get(URL_TEMPLATE)
 
 soup = BeautifulSoup(request.text, "html.parser")
-# print(soup.prettify())
 
-site_content = soup.find_all('div', class_='news')
+site_content = soup.find_all("div", class_="news")
 
 for new in site_content:
-    new_head_class = new.find('div', class_='news-head')
-    a_tags = new.find_all('a')
+    new_head_class = new.find("div", class_="news-head")
+    a_tags = new.find_all("a")
     print(new_head_class.text)
-    print('--------------')
-    for a_tag in a_tags:
-        str = '* ' + a_tag.find('div', class_='news_n').text.replace('\n', '').strip() + ' | ' + a_tag.find('span', class_='news_s').text.replace('\n', '').strip()
-        # str = a_tag.text
-        print(str)
-        
+    print("--------------")
 
-# print(len(site_content))
+    for a_tag in a_tags:
+        str = (
+            "* "
+            + a_tag.find("div", class_="news_n").text.replace("\n", "").strip()
+            + " | "
+            + a_tag.find("span", class_="news_s").text.replace("\n", "").strip()
+        )
+        print(str)
